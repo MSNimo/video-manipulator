81357dec7e9ca49bdcd92d1c6d9933c855f3f1e3
MSNimo <mikebenim@gmail.com>
2017-01-14 19:17:47 -0600

    Frontend and reception of parameters complete. Moving onto to scraping

--- app.py
+++ app.py
@@ -9,20 +9,40 @@ from flask import jsonify
 from flask import render_template
 import json
 import datetime
+import webbrowser
+ 
+app = Flask(__name__) 
 
-app = Flask(__name__)
-
-app.route('/', methods=['GET', 'POST'])
+error = 'none'
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	render_template('index.html')
+        render_template('index.html')
 	if request.method == 'POST':
 		url = request.form['link']
 		key = request.form['keyword']
 	        act = request.form['action']
-	return render_template('processing.html')	
+	        return editor (url, key, act)
+	return render_template('index.html')	
+  
+def editor(url, key, act):
 
+    website = 'youtube.com'
+    if website in url:
+        return "placeholder"
+    else:
+        error = "youtube link not provided"
+        return render_template('index.html', error = error)
+        
+    '''  
+    i = 0
+    while (i < 100000000):
+        i= i + 1 
+    return (key+act)
+   '''
+    
 if __name__ == '__main__':
         app.debug = True
         port = int(os.environ.get('PORT',5000))
         app.run(host='0.0.0.0', port=port)
 	#app.run(debug=True)
+	
